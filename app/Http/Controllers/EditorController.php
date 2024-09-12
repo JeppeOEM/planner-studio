7a7f1ca5f68ac2224f8661dce5acb82d06be4e11
Jeppe <jepp026b@stud.kea.dk>
2024-09-12 12:30:21 +0200

    configurator component created and inserted in editor

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
 class EditorController extends Controller
 {
-    function index(Request $request) {
+    function index(Request $request)
+    {
 
         return inertia('editor/editor');
     }
--- resources/js/Pages/editor/editor.vue
+++ resources/js/Pages/editor/editor.vue
@@ -1,5 +1,11 @@
 <template>
+
+<Head title="Editor" />
     <h1 class="text-4xl">Hej</h1>
+    <configurator />
 </template>
 
-<script setup></script>
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import configurator from "@/components/configurator.vue";
+</script>
