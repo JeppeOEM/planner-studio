935eb38f33435bd32a55c0487d96f8c134885f37
Jeppe <jepp026b@stud.kea.dk>
2024-09-12 13:06:50 +0200

    configurator basic setup

--- resources/js/components/configurator.vue
+++ resources/js/components/configurator.vue
@@ -1,5 +1,23 @@
 <template>
-    <h1>configurator</h1>
+    <div class="containerCanvas">
+        <canvas class="draw"></canvas>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const gltfloader = new GLTFLoader();
+const canvas = document.querySelector(".draw");
+let width = canvas.clientWidth;
+let height = canvas.clientHeight;
+let aspect = width / height;
+
+function onWindowResize() {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+}
+</script>
